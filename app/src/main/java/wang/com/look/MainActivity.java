9bcec6ec883d4d69de3129e5a04d5e67746588f0
WR-Android <641117356@qq.com>
2019-03-14 13:27:21 +0800

    解决了返回码302重定向问题

--- app/src/main/java/wang/com/look/MainActivity.java
+++ app/src/main/java/wang/com/look/MainActivity.java
@@ -3,7 +3,6 @@ package wang.com.look;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +12,7 @@ import android.widget.Toast;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "请求资源不存在", Toast.LENGTH_SHORT).show();
                     break;
                 case REQUESTEXCEPTION:
-                    Toast.makeText(getApplicationContext(),"服务器忙 请稍后...",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"错误地址",Toast.LENGTH_SHORT).show();
                     break;
                 default:
                     break;
@@ -63,18 +63,37 @@ public class MainActivity extends AppCompatActivity {
             try {
                 //获取源码地址
                 String path = et_path.getText().toString().trim();
+
                 //创建url对象指定我们要访问的网址（路径）
                 URL url = new URL(path);
+
                 //拿到http对象 用于发送或接收数据
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
                 //设置发送get请求
                 conn.setRequestMethod("GET"); //get要求大写 默认就是get请求
+
                 //设置请求的超时时间
                 conn.setConnectTimeout(5000);
+
                 //获取服务器返回的状态码
                 int code = conn.getResponseCode();
+
+                //获取头部信息，获取到重定向后的地址（为了解决返回码为302的问题）
+                if(code == 302){
+                    String location = conn.getHeaderField("Location");
+                    Log.d(TAG, "location " + location);
+                    //为URLConnection设置请求参数（请求方式，连接的超时时间等）
+                    url  = new URL(location);
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(5000);
+                    code = conn.getResponseCode();
+                }
+
                 //如果code==200说明请求成功
                 if(code==200){
+                    Log.d(TAG, "run: 777");
                     //获取服务器返回的数据 是以流的形式返回的
                     //由于把流转换成字符串是一个非常常见的操作 所以我们抽出一个工具类（utils）
                     InputStream in = conn.getInputStream();
@@ -92,19 +111,19 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     //请求资源不存在 Toast是一个View 也不能在子线程更新UI
                     Message msg = new Message();
-                    msg.what = REQUESTNOYFOUND; //代表哪条条消息
+                    msg.what = REQUESTNOYFOUND;
                     handler.sendMessage(msg);
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
                 Message msg = new Message();
-                msg.what = REQUESTEXCEPTION; //代表哪条条消息
+                msg.what = REQUESTEXCEPTION; //错误地址
                 handler.sendMessage(msg);
 
             }
 
-        };}.start();
+        }}.start();
 
     }
 
