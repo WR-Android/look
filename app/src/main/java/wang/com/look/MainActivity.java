d0f01feec399003005483a44a5a5abe8678570dd
WR-Android <641117356@qq.com>
2019-03-15 09:50:57 +0800

    添加网络图片缓存在本地

--- app/src/main/java/wang/com/look/MainActivity.java
+++ app/src/main/java/wang/com/look/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -13,7 +14,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUESTNOYFOUND = 1;
     public static final int REQUESTEXCEPTION = 2;
     public static final int IMAGEIN = 3;
+    public static final int IMAGEIN_CACHE = 4;
+
     private static final String TAG = "MainActivity";
     private ImageView iv;
     private EditText et_path;
@@ -49,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
                     iv.setImageBitmap(bitmap);
                     Toast.makeText(getApplicationContext(), "图片显示成功", Toast.LENGTH_SHORT).show();
                     break;
+                case IMAGEIN_CACHE:
+                    iv.setImageBitmap((Bitmap) msg.obj);
+                    Toast.makeText(getApplicationContext(), "缓存图片显示成功", Toast.LENGTH_SHORT).show();
+                    break;
                 default:
                     break;
             }
@@ -71,36 +79,59 @@ public class MainActivity extends AppCompatActivity {
             try {
                 //获得访问图片路径
                 String path = et_path.getText().toString().trim();
-                //创建url对象指定我们要访问的网址（路径）
-                URL url = new URL(path);
 
-                //拿到http对象 用于发送或接收数据
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-                //设置发送get请求
-                conn.setRequestMethod("GET"); //get要求大写 默认就是get请求
-
-                //设置请求的超时时间
-                conn.setConnectTimeout(5000);
-
-                //获取服务器返回的状态码
-                int code = conn.getResponseCode();
-
-                if(code==200){
-
-                    //获取图片数据 不管什么数据都是以流的形式返回
-                    InputStream in = conn.getInputStream();
-
-                    //通过位图工厂获取bitmap
-                    Bitmap bitmap = BitmapFactory.decodeStream(in);
-
-                    Message msg = Message.obtain(); //使用msg的静态方法可以减少对象的创建
-                    msg.obj = bitmap;
-                    msg.what = IMAGEIN;
+                File file = new File(getCacheDir(), Base64.encodeToString(path.getBytes(),Base64.DEFAULT));
+                if(file.exists() && file.length()>0){
+                    //使用缓存图片
+                    Bitmap cachebitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                    Message msg = Message.obtain();
+                    msg.obj = cachebitmap;
+                    msg.what = IMAGEIN_CACHE;
                     handler.sendMessage(msg);
-                    in.close();
-                }
+                    Toast.makeText(getApplicationContext(), "显示缓存图片", Toast.LENGTH_SHORT).show();
+                }else {
+                    //第一次访问网络获取数据
 
+                    //创建url对象指定我们要访问的网址（路径）
+                    URL url = new URL(path);
+
+                    //拿到http对象 用于发送或接收数据
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    //设置发送get请求
+                    conn.setRequestMethod("GET"); //get要求大写 默认就是get请求
+
+                    //设置请求的超时时间
+                    conn.setConnectTimeout(5000);
+
+                    //获取服务器返回的状态码
+                    int code = conn.getResponseCode();
+
+                    if (code == 200) {
+
+                        //获取图片数据 不管什么数据都是以流的形式返回
+                        InputStream in = conn.getInputStream();
+
+                        //缓存图片 Google提供了一个缓存目录
+
+                        FileOutputStream fos = new FileOutputStream(file);
+                        int len = -1;
+                        byte[] buffer = new byte[1024];
+                        while ((len = in.read(buffer)) != -1) {
+                            fos.write(buffer, 0, len);
+                        }
+                        fos.close();
+                        in.close();
+
+                        //通过位图工厂获取bitmap
+                        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+                        Message msg = Message.obtain(); //使用msg的静态方法可以减少对象的创建
+                        msg.obj = bitmap;
+                        msg.what = IMAGEIN;
+                        handler.sendMessage(msg);
+                    }
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
